e5733d42f2dd4330232ef1bc050e3e86e8f7400e
Prof Edir <ecaramba@gmail.com>
2024-06-13 20:53:18 -0300

    listando alunos

--- alunos/front/alunos.js
+++ alunos/front/alunos.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
 
     $.getJSON("http://localhost:3003/alunos", function(dados){
 
-        console.log(dados);
+        $("#lista-alunos").empty();
+
+        dados.forEach(function(aluno){
+            let tr = "<tr>"
+                    +"<td>"+ aluno.nome +"</td>"
+                    +"<td>"+ aluno.email +"</td>"
+                    +"<td>"+ aluno.cidade +"</td>"
+                    +"<td>"+ aluno.telefone +"</td>"
+                +"</tr>";
+
+            $("#lista-alunos").append(tr);
+        });
 
     }); //fim do getJson
 
