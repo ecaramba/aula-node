84de793052d473e1186a94a105061147afda3cef
Prof Edir <ecaramba@gmail.com>
2024-06-19 20:18:42 -0300

    criado model

---
+++ petvet/back/model/servico.js
@@ -0,0 +1,41 @@
+// CRUD
+// Create -> Cadastrar
+// Read -> Ler
+// Update -> alterar
+// Delete -> Deletar
+
+/**
+ * Cadastra um novo serviço
+ * @param {object} dados 
+ */
+function cadastrar(dados)
+{
+
+}
+
+/**
+ * Deleta o serviço informado
+ * @param {string} id 
+ */
+function deletar(id)
+{
+
+}
+
+/**
+ * Altera o registro com o valor novo
+ * @param {string} id 
+ * @param {object} novo 
+ */
+function alterar(id, novo)
+{
+
+}
+
+/**
+ * Lista todos os serviços
+ */
+function listar()
+{
+
+}
---
+++ petvet/back/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(3003, function(){
+    console.log("servidor iniciado");
+});
+
+// MVC
+// Model -> dados
+// View -> visual -> frontend
+// Controller -> controlador -> regras de negocio
+
