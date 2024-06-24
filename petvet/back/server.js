cebda7e4054a1950dd81962ead930ef37632dc1b
Prof Edir <ecaramba@gmail.com>
2024-06-24 20:20:59 -0300

    ambiente env

--- petvet/back/server.js
+++ petvet/back/server.js
@@ -4,14 +4,27 @@ const cors = require("cors");
 
 const servico = require("./controller/servico");
 
+const env = process.env.AMBIENTE;
+
+// let porta = "";
+// if (env == "DEV") {
+//     porta = 3003;
+// } else {
+//     porta = 80;
+// }
+// if ternario
+let porta = (env == "DEV")? 3003 : 80;
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/servico", servico);
 
-app.listen(3003, function(){
+app.listen(porta, function(){
     console.log("servidor iniciado");
+    console.log(env);
 });
 
 // MVC
